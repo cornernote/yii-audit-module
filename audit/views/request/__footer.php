8e818c89bda12db91768adcd4a60a6b7403f430a
cornernote <cornernote@gmail.com>
2013-12-23 01:31:00 +1030

    allow htmlOptions

--- audit/views/request/__footer.php
+++ audit/views/request/__footer.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * @var $this CController
- * @var $color string
+ * @var $tag string
+ * @var $htmlOptions array
  *
  * @author Brett O'Donnell <cornernote@gmail.com>
  * @author Zain Ul abidin <zainengineer@gmail.com>
@@ -12,13 +13,22 @@
  * @package yii-audit-module
  */
 
-$output = array();
+if (!isset($tag))
+    $tag = 'span';
+
+if (!isset($htmlOptions))
+    $htmlOptions = array(
+        'style' => 'color:' . (isset($color) ? $color : (YII_DEBUG || $isAdmin ? 'inherit' : 'transparent')) . ';',
+    );
+
 $app = Yii::app();
 /** @var AuditModule $audit */
 $audit = $app->getModule('audit');
 $isAdmin = in_array($app->getUser()->name, $audit->adminUsers);
 /** @var AuditErrorHandler $errorHandler */
 $errorHandler = $app->getErrorHandler();
+
+$output = array();
 if ($errorHandler->hasAuditRequest()) {
     $auditRequest = $errorHandler->getAuditRequest();
     if ($isAdmin)
@@ -30,6 +40,4 @@ $output[] = number_format(microtime(true) - YII_BEGIN_TIME, 2) . 's';
 $output[] = round(memory_get_peak_usage() / 1024 / 1024, 1) . 'm';
 $output[] = date('Y-m-d H:i:s');
 
-echo CHtml::tag('span', array(
-    'style' => 'color:' . (isset($color) ? $color : (YII_DEBUG || $isAdmin ? 'inherit' : 'transparent')) . ';',
-), implode(' | ', $output));
+echo CHtml::tag($tag, $htmlOptions, implode(' | ', $output));
