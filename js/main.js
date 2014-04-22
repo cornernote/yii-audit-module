d0a98955a5c6617a07fd986c7e8266a37e8fc24d
Brett O'Donnell <cornernote@gmail.com>
2014-04-22 12:09:20 +0930

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
         dataType: 'json',
         success: function(result) {
             for(i in result) {
-                $('#contributors ul').append('<li class="thumbnail"><a href="' + result[i].html_url + '" target="_blank"><img src="' + result[i].avatar_url + 's=64" title="' + result[i].login + ' (' + result[i].contributions + ' contributions)"></a></li>');
+                $('#contributors ul').append('<li class="thumbnail"><a href="https://github.com/cornernote/yii-audit-module/commits?author=' + result[i].login + '" target="_blank"><img src="' + result[i].avatar_url + 's=64" title="' + result[i].login + ' (' + result[i].contributions + ' contributions)"></a></li>');
             }
             $('#contributors h5').html(result.length + ' developer'+ (result.length>1 ? 's' : '') + ' contributed to this project:');
         }
