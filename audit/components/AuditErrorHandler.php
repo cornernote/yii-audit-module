c5914e535072ce2dea3d56e52e8d4770e0654b5d
Brett O'Donnell <cornernote@gmail.com>
2022-07-05 11:30:20 +0930

    response_headers need to be packed
    incase they are an empty array

--- audit/components/AuditErrorHandler.php
+++ audit/components/AuditErrorHandler.php
@@ -483,8 +483,8 @@ class AuditErrorHandler extends CErrorHandler
                 }
             }
             $auditRequest->response_headers = $this->removeValuesWithPasswordKeys($auditRequest->response_headers);
-            $auditRequest->response_headers = AuditHelper::pack($auditRequest->response_headers);
         }
+        $auditRequest->response_headers = AuditHelper::pack($auditRequest->response_headers);
 
         $auditRequest->user_id = Yii::app()->user->id;
         $auditRequest->memory_usage = memory_get_usage();
